5df903d3e84b6476169fda4374db4fa3ea4efa05
erbynn <john.erbynn@gmail.com>
2019-11-04 10:15:28 +0000

    collection added

--- src/main/java/io/turntabl/ClientRegister.java
+++ src/main/java/io/turntabl/ClientRegister.java
@@ -1,5 +1,19 @@
 package io.turntabl;
 
+import jdk.internal.ref.Cleaner;
+
+import java.util.List;
+import java.util.Set;
+
 public class ClientRegister {
 
+    private List<Client> clients;
+
+    public ClientRegister(List<Client> clients) {
+        this.clients = clients;
+    }
+
+    public List<Client> getAllClientNames(){
+        return clients;
+    }
 }
