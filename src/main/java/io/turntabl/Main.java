90efb503a4c4309ff6b65a78ee9240d5efde1fd9
Erbynn <john.erbynn@gmail.com>
2019-11-13 22:02:36 +0000

    prod name and sales display nice

--- src/main/java/io/turntabl/Main.java
+++ src/main/java/io/turntabl/Main.java
@@ -19,10 +19,12 @@ public class Main {
 
         ProductDAOImpl productDAO = new ProductDAOImpl();
 //        productDAO.showAllProductsByCustomer("Karin Josephs");
-        for (ProductTO prod : productDAO.showAllProductsByCustomer("Karin Josephs")) {
-            System.out.printf("%10s %50s", prod.getProductName(), prod.getPrice());
-            System.out.println();
-        }
+//        for (ProductTO prod : productDAO.showAllProductsByCustomer("Karin Josephs")) {
+//            System.out.printf("%10s %50s", prod.getProductName(), prod.getPrice());
+//            System.out.println();
+//        }
+        productDAO.showAllProductsByCustomer("Karin Josephs").stream()
+                .forEach(c -> System.out.printf( "%30s %20s", c.getProductName(), c.getPrice() + "\n"));
 
 
 
