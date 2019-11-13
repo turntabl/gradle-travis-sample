43b4c9566c898e5ebec37755ab1de56b7f99d486
Erbynn <john.erbynn@gmail.com>
2019-11-13 16:46:37 +0000

    prepared statement for Category table

--- src/main/java/io/turntabl/JDBC.java
+++ src/main/java/io/turntabl/JDBC.java
@@ -1,5 +1,7 @@
 package io.turntabl;
 
+import org.graalvm.compiler.debug.CSVUtil;
+
 import java.sql.*;
 
 public class JDBC {
@@ -12,12 +14,12 @@ public class JDBC {
         String dbUrl = "jdbc:postgresql:northwind";
 
         // get the connection object
-        try (Connection db = DriverManager.getConnection(dbUrl, "john-erbynn", "turntabl")) {
+        try (Connection dbCon = DriverManager.getConnection(dbUrl, "john-erbynn", "turntabl")) {
             // obtain statement object
-            Statement s = db.createStatement();
+            Statement s = dbCon.createStatement();
 
             // statement execution with cursor
-            ResultSet rs = s.executeQuery("select * from customers limit 5; ");
+            ResultSet rs = s.executeQuery("select * from customers limit 20; ");
 
             // headings
             System.out.println("*****************************************************************************************************************************************");
@@ -33,15 +35,90 @@ public class JDBC {
                         rs.getString("city"),
                         rs.getString("region"),
                         rs.getString("country"),
-                        rs.getString("phone"));
+                        rs.getString("phone")
+                );
+                System.out.println();
             }
+
         } catch (SQLException sqle) {
             System.err.println("Connection error: " + sqle);
         }
 
+    }
 
+    public static void searchCustomerName(String name) throws ClassNotFoundException {
+        // load the JDBC driver from prostgresql
+        Class.forName("org.postgresql.Driver");
+
+        // build the connection string
+        String dbUrl = "jdbc:postgresql:northwind";
+
+        // get the connection object
+        try (Connection db = DriverManager.getConnection(dbUrl, "john-erbynn", "turntabl")) {
+            // obtain statement object
+            Statement s = db.createStatement();
+
+            // statement execution with cursor
+            ResultSet rs = s.executeQuery("select * from customers where contact_name ='" + name +"'; ");
+
+            // headings
+            System.out.println("*****************************************************************************************************************************************");
+            System.out.printf("%10s %10s %30s %1s %10s", "CONTACT_NAME", "TITLE", "CITY", "COUNTRY", "PHONE \n");
+            System.out.println("*****************************************************************************************************************************************");
+
+            // result manipulation
+            while (rs.next()) {
+                System.out.format("%10s %10s %30s %1s %10s",
+                        rs.getString("contact_name"),
+                        rs.getString("contact_title"),
+                        rs.getString("city"),
+                        rs.getString("country"),
+                        rs.getString("phone"));
+            }
+            System.out.println();
+
+        } catch (SQLException sqle) {
+            System.err.println("Connection error: " + sqle);
+        }
 
     }
+
+
+    // using prepared statement
+    public static void searchCategoryByName(String name) throws ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        String dbUrl = "jdbc:postgresql:northwind";
+        try (Connection db = DriverManager.getConnection(dbUrl, "john-erbynn", "turntabl")) {
+            PreparedStatement preparedStatement = db.prepareStatement("select * from categories where category_name = ?; ");
+            preparedStatement.clearParameters();
+
+//            preparedStatement.setString(1, "Beverages");
+            preparedStatement.setString(1, name);   // => name matched to the first ?
+
+            ResultSet result = preparedStatement.executeQuery();
+
+            System.out.println("*****************************************************************************************************************************************");
+            System.out.printf("%10s %60s", "CATEGORY_NAME", "DESCRIPTION\n");
+            System.out.println("*****************************************************************************************************************************************");
+
+            // result manipulation
+            while (result.next()) {
+                System.out.format("%2s %70s",
+                        result.getString("category_name"),
+                        result.getString("description")
+                );
+                System.out.println();
+            }
+
+        } catch (SQLException sqle) {
+            System.err.println("Connection error: " + sqle);
+        }
+
+    }
+
+    // TODO: create prepared statement for Product and Employee name searches
+
+
 
 
 }
--- src/main/java/io/turntabl/Main.java
+++ src/main/java/io/turntabl/Main.java
@@ -1,9 +1,21 @@
 package io.turntabl;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException {
         JDBC jdbc = new JDBC();
-        jdbc.jdbc_read_data();
+//        jdbc.jdbc_read_data();
+
+        /*System.out.println("Enter name: >>> ");
+        Scanner inp = new Scanner(System.in);
+        String name = inp.nextLine();
+        jdbc.searchCustomerName(name);*/
+
+        jdbc.searchCategoryByName("Beverages");
+//        jdbc.searchCategoryByName();
+
+
     }
 }
