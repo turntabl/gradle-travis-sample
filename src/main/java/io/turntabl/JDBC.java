e3b000eec62eea3386eb1703e5bfa50096afce08
Erbynn <john.erbynn@gmail.com>
2019-11-13 13:16:30 +0000

    pretty print working

--- src/main/java/io/turntabl/JDBC.java
+++ src/main/java/io/turntabl/JDBC.java
@@ -3,7 +3,8 @@ package io.turntabl;
 import java.sql.*;
 
 public class JDBC {
-    public static void jdbcConnection() throws ClassNotFoundException {
+
+    public static void jdbc_read_data() throws ClassNotFoundException {
         // load the JDBC driver from prostgresql
         Class.forName("org.postgresql.Driver");
 
@@ -11,12 +12,35 @@ public class JDBC {
         String dbUrl = "jdbc:postgresql:northwind";
 
         // get the connection object
-
         try (Connection db = DriverManager.getConnection(dbUrl, "john-erbynn", "turntabl")) {
+            // obtain statement object
+            Statement s = db.createStatement();
 
+            // statement execution with cursor
+            ResultSet rs = s.executeQuery("select * from customers limit 5; ");
+
+            // headings
+            System.out.println("*****************************************************************************************************************************************");
+            System.out.printf("%10s %10s %30s %1s %10s %10s %10s", "CONTACT NAME", "CONTACT TITLE", "ADDRESS", "CITY", "REGION", "COUNTRY", "PHONE \n");
+            System.out.println("*****************************************************************************************************************************************");
+
+            // result manipulation
+            while (rs.next()) {
+                System.out.format("%10s %10s %30s %1s %10s %10s %10s",
+                        rs.getString("contact_name"),
+                        rs.getString("contact_title"),
+                        rs.getString("address"),
+                        rs.getString("city"),
+                        rs.getString("region"),
+                        rs.getString("country"),
+                        rs.getString("phone"));
+            }
         } catch (SQLException sqle) {
             System.err.println("Connection error: " + sqle);
         }
+
+
+
     }
 
 
--- src/main/java/io/turntabl/Main.java
+++ src/main/java/io/turntabl/Main.java
@@ -2,7 +2,8 @@ package io.turntabl;
 
 public class Main {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws ClassNotFoundException {
+        JDBC jdbc = new JDBC();
+        jdbc.jdbc_read_data();
     }
 }
