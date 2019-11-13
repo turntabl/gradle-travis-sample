f1bbeb3d7a48f00885a3ec1df2b2bcd97c028e1a
Erbynn <john.erbynn@gmail.com>
2019-11-13 20:56:57 +0000

    new line

--- src/main/java/io/turntabl/Main.java
+++ src/main/java/io/turntabl/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException {
         JDBC jdbc = new JDBC();
+
 //        jdbc.jdbc_read_data();
 
         /*System.out.println("Enter name: >>> ");
