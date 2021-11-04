166225e11d4bbf710eb2a97fd162b5562595b72c
Mickey D. Luffy <thefalconslayer@gmail.com>
2021-11-04 02:09:09 +0000

    Gradle run test not working!! Will ask Sam tomorow

--- src/test/java/NumbersTest.java
+++ src/test/java/NumbersTest.java
@@ -1,5 +1,4 @@
-package io.turntabl;
-
+import io.turntabl.Numbers;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,8 +7,6 @@ public class NumbersTest {
 
     @Test
     public void testLessThanZero() {
-
-
         System.out.println("Running the first test");
         boolean result = Numbers.isGreaterThanZeroAndLessThanAThousand(-1);
         assertFalse(result);
@@ -17,18 +14,24 @@ public class NumbersTest {
 
     @Test
     public void testZero() {
+        System.out.println("==============Test Zero===========");
+
         boolean result = Numbers.isGreaterThanZeroAndLessThanAThousand(0);
         assertFalse(result);
     }
 
     @Test
     public void testGreaterThanZero() {
+        System.out.println("==============testGreaterThanZero===========");
+
         boolean result = Numbers.isGreaterThanZeroAndLessThanAThousand(1);
         assertTrue(result);
     }
 
     @Test
     public void testLessThanAThousand() {
+        System.out.println("==============testLessThanAThousand===========");
+
         boolean result = Numbers.isGreaterThanZeroAndLessThanAThousand(999);
         assertTrue(result);
     }
---
+++ src/test/java/Testing.java
@@ -0,0 +1,7 @@
+import org.junit.Test;
+public class Testing {
+    @Test
+    public void test() {
+        System.out.println("work pls");
+    }
+}
