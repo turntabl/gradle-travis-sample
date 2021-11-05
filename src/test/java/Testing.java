b735c00ef51955a51fda4a8beb46c65a9420c46a
MickeyDLuffy <thefalconslayer@gmail.com>
2021-11-05 10:27:10 +0000

    Conflict

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -1,7 +1,14 @@
+import io.turntabl.domain.PrivateClient;
+import io.turntabl.domain.ServiceLevel;
 import org.junit.Test;
 public class Testing {
     @Test
     public void test() {
-        System.out.println("work pls");
+        PrivateClient privateClient = new PrivateClient("Mickey", ServiceLevel.GOLD, "Lady P");
+
+        System.out.println(privateClient.save("micke", String::length));
+
+        System.out.println(privateClient.useSupplier(() -> 50));
+
     }
 }
--- src/test/java/io/turntabl/domain/PrivateClient.java
+++ src/test/java/io/turntabl/domain/PrivateClient.java
@@ -1,9 +1,33 @@
 package io.turntabl.domain;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 public class PrivateClient extends Client{
     private String clientName;
     public PrivateClient(String name, ServiceLevel serviceLevel, String clientName) {
         super(name, serviceLevel);
         this.clientName = clientName;
+        System.out.println(save("micke", String::length));
+        System.out.println(get(String::concat));
     }
+
+    public int save(String s, Function<String, Integer> saveMani) {
+        return saveMani.apply(s);
+    }
+
+    public String get(BiFunction<String, String, String> concat) {
+        return concat.apply("Mickey", "Loo");
+    }
+
+    public String useSupplier(Supplier<Integer> supply) {
+
+       return supply.get().toString();
+
+    }
+
+
+
 }
