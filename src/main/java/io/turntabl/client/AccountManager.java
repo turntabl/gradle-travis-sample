abb40cbe54bfa1b91cc7bb9fa5b8d3278dddc3bc
benneth-yankey <benneth.yankey@turntabl.io>
2021-11-06 23:43:10 +0000

    changed from abstract class to interface

--- src/main/java/io/turntabl/client/AccountManager.java
+++ src/main/java/io/turntabl/client/AccountManager.java
@@ -1,18 +1,7 @@
 package io.turntabl.client;
 
-public class AccountManager {
-    private String name;
-
-    public AccountManager(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+public interface AccountManager {
+    String getName();
+    void setName(String name);
 }
 
